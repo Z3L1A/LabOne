0eb584edb6e0eb2ba88741687ca8e4d33607a827
Vlad Zelinskiy <vldzelinskiy@gmail.com>
2020-01-23 17:40:21 +0200

    Add project files.

---
+++ LabOne/ExpressionCalculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LabOne
+{
+    public static class ExpressionCalculator
+    {
+        private static readonly Func<double, double, double, double, double> First = (a, b, c, d) => 
+            Math.Log(Math.Abs(2 * a), Math.E) / 3 * Math.Tan(b) - Math.Sqrt(c / Math.Sqrt(Math.Abs(d)));
+
+        private static readonly Func<double, double, double, double, double> Second = (a, b, c, d) =>
+            2 * Math.Sqrt(Math.Sin(a) / Math.Abs(Math.Tan(b - a)) + Math.Log(c, Math.E) / d);
+
+        private static readonly Func<double, double, double, double, double> Third = (a, b, c, d) =>
+            2 * Math.Sqrt(Math.Tan(a) / Math.Abs(Math.Acos(b))) - 3 * Math.Cbrt(Math.Pow(Math.E, c - a) * Math.Sinh(d));
+
+        public static double GetResultFromFirstExpression() => First(-2.86, 1.62, 10.874, -2.91);
+
+        public static double GetResultFromSecondExpression() => Second(1.54, 0.49, 24.1, 0.87);
+
+        public static double GetResultFromThirdExpression() => Third(1.25, 3.09, 4.25, 0.56);
+
+        public static void ToConsole(this double result)
+        {
+            Console.WriteLine($"\nРезультат вычислений: {result}");
+        }
+    }
+}
---
+++ LabOne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+// Variant 5
+namespace LabOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputKey = ConsoleKey.Enter;
+
+            while (inputKey != ConsoleKey.Escape)
+            {
+                Console.WriteLine("Введите номер уравнения для расчета: (для выхода нажмите Esc)");
+                inputKey = Console.ReadKey().Key;
+
+                switch (inputKey)
+                {
+                    case ConsoleKey.D1:
+                        ExpressionCalculator.GetResultFromFirstExpression().ToConsole();
+                        break;
+                    case ConsoleKey.D2:
+                        ExpressionCalculator.GetResultFromSecondExpression().ToConsole();
+                        break;
+                    case ConsoleKey.D3:
+                        ExpressionCalculator.GetResultFromThirdExpression().ToConsole();
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        Console.WriteLine("\nВведите корректный символ.");
+                        break;
+                }
+            }
+        }
+    }
+}
